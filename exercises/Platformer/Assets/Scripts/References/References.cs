62be7d8867a0a3e785aed48d5fb8f6fed7ee6abd
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2024-03-04 13:50:02 -0500

    Platformer - Prototype 1 References
    Added my consulted videos for the week 1 prototype involving mirrors, and player/camera movement.

---
+++ exercises/Platformer/Assets/Scripts/References/References.cs
@@ -0,0 +1,9 @@
+/* References
+ * 
+ * Mirrors - "How To Create Mirrors in Unity3D" by Developer Jake - https://www.youtube.com/watch?v=4v3eAhuleI0
+ * Player Movement - "Making a 3D Platformer in Unity 3D Engine - Player Movement in C#" by Kozmobot Games - https://www.youtube.com/watch?v=F7bdwOFofLk
+ *                   "Making a 3D Platformer in Unity - Player Rotation in C#" by Kozmobot Games - https://www.youtube.com/watch?v=v6Kh748AwJU
+ *                   "Making a 3D Platformer in Unity - Player Jump in C#" by Kozmobot Games - https://www.youtube.com/watch?v=OazVLjSNJiQ
+ * Camera Movement - "3D Platformer in Unity - CAMERA Follow Tutorial" by Kozmobot Games - https://www.youtube.com/watch?v=etmD15gsgCM
+ *                   
+*/
